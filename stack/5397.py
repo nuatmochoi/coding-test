d83efd9902adb61cd34411c321c7a416f0b21dba
nuatmochoi <tjddn8770@naver.com>
2020-02-09 21:00:08 +0900

    Baekjoon algorithm #5397

---
+++ stack/5397.py
@@ -0,0 +1,18 @@
+for _ in range(int(input())):
+    l_stack = []
+    r_stack = []
+    data= input()
+    for i in data:
+        if i == '-':
+            if l_stack:
+                l_stack.pop()
+        elif i == '<':
+            if l_stack:
+                r_stack.append(l_stack.pop())
+        elif i == '>':
+            if r_stack:
+                l_stack.append(r_stack.pop())
+        else:
+            l_stack.append(i)
+    l_stack.extend(reversed(r_stack))
+    print(''.join(l_stack))
